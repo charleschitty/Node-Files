c7f15b69d42746c6bff50ee367ccbc85c097f2ac
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-02 17:26:03 -0600

    before code review

--- step2.js
+++ step2.js
@@ -5,31 +5,33 @@ const fsP = require("fs/promises");
 /** Receives a path to a file as an input, reads the file, and prints the
  * contents of that file or the error received if it fails to read said file.
  */
-async function cat(path){
-  try  {
-    let contents = await fsP.readFile(path, "utf8");
-    console.log("file contents: ", contents);
+async function cat(path) {
+  try {
+
+    const contents = await fsP.readFile(path, "utf8");
+    console.log("File Content: ", contents);
 
   } catch (err) {
-    console.log(`Error reading ${path}: ${err}`);
+
+    console.error(`Error reading ${path}: ${err}`);
     process.exit(1);
   }
 }
 
-// cat(process.argv[2]);
+/** Receives an url, if valid URL, print content of URL,
+ * print error if invalid URL*/
 
-/** Receives an url, if valid URL, print content of URL . */
+async function webCat(url) {
+  try {
+    const response = await fetch(url);
+    const respInHTML = await response.text();
+    console.log("URL Content: ", respInHTML);
 
-async function webCat(url){
-  try{
-  const response = await fetch(url);
-  let respInHTML = await response.text();
-  console.log("Content: ", respInHTML);
-  } catch(err) {
-    console.log(`Error fetching ${url}: ${err}`);
+  } catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
     process.exit(1);
-
   }
 }
-let path = process.argv[2];
-URL.canParse(path) ? webCat(path) : cat(path);
+
+const PATH = process.argv[2];
+URL.canParse(PATH) ? webCat(PATH) : cat(PATH);
